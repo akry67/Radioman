ac2fcc8426e58b20113d682329bb06c1fb0c929d
Anna Kuznetsova <akry67@gmail.com>
2021-11-17 22:01:08 +0400

    100%

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,78 +4,89 @@ public class Radio {
     public int currentRadioStation;
     public int currentVolume;
 
-    public void setCurrentRadioStation(int currentRadioStation){
+    public void setCurrentRadioStation(int currentRadioStation) {
         //диапазон от 0 до 9 радиостанция
-        if (currentRadioStation > 9){
+        if (currentRadioStation > 9) {
             return;
         }
-        if (currentRadioStation < 0){
+        if (currentRadioStation < 0) {
             return;
         }
         this.currentRadioStation = currentRadioStation;
     }
 
-    public void nextCurrentRadioStation(int currentRadioStation){
-        if (currentRadioStation > 9){
+    public void nextCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 9) {
             this.currentRadioStation = 0;
         }
         if (currentRadioStation < 0) {
             return;
         }
-        if (currentRadioStation < 9){
-            this.currentRadioStation = currentRadioStation + 1;
+            if (currentRadioStation < 9) {
+                this.currentRadioStation = currentRadioStation + 1;
+            }
         }
-    }
 
-    public void prevCurrentRadioStation(int currentRadioStation){
-        if (currentRadioStation <= 0){
-            this.currentRadioStation = 9;
+        public void prevCurrentRadioStation ( int currentRadioStation){
+            if (currentRadioStation <= 0) {
+                this.currentRadioStation = 9;
+            }
+            if (currentRadioStation > 9) {
+                return;
+            }
+            if (currentRadioStation >= 1) {
+                this.currentRadioStation = currentRadioStation - 1;
+            }
         }
-        if (currentRadioStation > 9) {
-            return;
-        }
-        if (currentRadioStation >= 1){
-            this.currentRadioStation = currentRadioStation - 1;
-        }
-    }
 
-    public int getCurrentRadioStation() {
-        //getCurrentRadioStation - дай текущую радиостанцию
-        return currentRadioStation;
-    }
+        public int getCurrentRadioStation () {
+            //getCurrentRadioStation - дай текущую радиостанцию
+            return currentRadioStation;
+        }
 
-    public void moreVolume(int currentVolume){
-        if (currentVolume < 0){
-            return;
+        public void moreVolume ( int currentVolume){
+            if (currentVolume < 0) {
+                return;
+            }
+            if (currentVolume < 10) {
+                this.currentVolume = currentVolume + 1;
+            }
         }
-        if (currentVolume < 10){
-            this.currentVolume = currentVolume +1;
-        }
-    }
 
-    public void underVolume(int currentVolume){
-        if (currentVolume > 10){
-            return;
+        public void underVolume ( int currentVolume){
+            if (currentVolume > 10) {
+                return;
+            }
+            if (currentVolume > 0) {
+                this.currentVolume = currentVolume - 1;
+            }
         }
-        if (currentVolume > 0){
-            this.currentVolume = currentVolume - 1;
-        }
-    }
 
-    public void maxVolume(){
-        if (currentVolume > 10){
-            return;
+        public void maxVolume ( int currentVolume){
+            if (currentVolume > 10) {
+                return;
+            }
+        }
+
+        public void minVolume ( int currentVolume){
+            if (currentVolume < 0) {
+                return;
+            }
+        }
+
+        public void setCurrentVolume ( int currentVolume){
+            //диапазон от 0 до 10 громкости
+            if (currentVolume > 10) {
+                return;
+            }
+            if (currentVolume < 0) {
+                return;
+            }
+            this.currentVolume = currentVolume;
+        }
+
+        public int getCurrentVolume () {
+            // дай текущую громкость
+            return currentVolume;
         }
     }
-
-    public void minVolume(){
-        if (currentVolume < 0){
-            return;
-        }
-    }
-
-    public int getCurrentVolume() {
-        // дай текущую громкость
-        return currentVolume;
-    }
-}
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,7 +8,7 @@ class RadioTest {
 
     @Test
     //текущая радиостанция
-    public void getCurrentRadioStation(){
+    public void getCurrentRadioStation() {
         Radio radio = new Radio();
         radio.setCurrentRadioStation(1);
 
@@ -20,11 +20,11 @@ class RadioTest {
 
     @Test
     //следующая радиостанция
-    public void nextCurrentRadioStation(){
+    public void nextCurrentRadioStation() {
         Radio radio = new Radio();
-        radio.nextCurrentRadioStation(9);
+        radio.nextCurrentRadioStation(-1000);
 
-        int expected =0;
+        int expected = 0;
         int actual = radio.currentRadioStation;
 
         assertEquals(expected, actual);
@@ -32,11 +32,11 @@ class RadioTest {
 
     @Test
     //предыдущая радиостанция
-    public void prevCurrentRadioStation(){
+    public void prevCurrentRadioStation() {
         Radio radio = new Radio();
-        radio.prevCurrentRadioStation(7);
+        radio.prevCurrentRadioStation(1);
 
-        int expected =6;
+        int expected = 0;
         int actual = radio.currentRadioStation;
 
         assertEquals(expected, actual);
@@ -44,11 +44,11 @@ class RadioTest {
 
     @Test
     //увеличение звука
-    public void moreVolume(){
+    public void moreVolume() {
         Radio radio = new Radio();
-        radio.moreVolume(-2);
+        radio.moreVolume(-1);
 
-        int expected =0;
+        int expected = 0;
         int actual = radio.currentVolume;
 
         assertEquals(expected, actual);
@@ -56,12 +56,51 @@ class RadioTest {
 
     @Test
     //уменьшение звука
-    public void underVolume(){
+    public void underVolume() {
         Radio radio = new Radio();
-        radio.underVolume(10);
+        radio.underVolume(2);
 
-        int expected = 9;
+        int expected = 1;
         int actual = radio.currentVolume;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    //текущая громкость
+    public void getCurrentVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(11);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    //макс громкость
+    public void maxCurrentVolume() {
+        Radio radio = new Radio();
+        radio.maxVolume(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    //мин громкость
+    public void minCurrentVolume() {
+        Radio radio = new Radio();
+        radio.minVolume(0);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
 
         assertEquals(expected, actual);
 
